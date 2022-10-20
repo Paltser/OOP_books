c34f81e70510dab521454aa9b7bd29965305ea45
markos <markos.paltser@voco.ee>
2022-10-20 09:54:46 +0300

    remove 1 book from ui and ls + read ls when reload page

--- Classes/UI.js
+++ Classes/UI.js
@@ -14,6 +14,21 @@ class UI {
                 <td><a href="#">X</a></td>`
         this.bookList.appendChild(tr)
     }
+    getBook(click) {
+        let isbn = click.parentElement.previousElementSibling.textContent
+        let author = click.parentElement.previousElementSibling.previousElementSibling.textContent
+        let title = click.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.textContent
+        const book = new Book(title, author, isbn)
+        return book
+    }
+    delBook(click){
+        if (confirm('really wanna delete book?')){
+            click.parentElement.parentElement.remove()
+            return true
+        }else {
+            return false
+        }
+    }
 
 
 }
--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const ls = new LS()
 const subbtn = document.getElementById('subbtn')
 subbtn.addEventListener("click", addBook)
 
+document.addEventListener("DOMContentLoaded", getBooks)
+
+const bookList = document.querySelector('#book-list')
+bookList.addEventListener('click', delBook)
+
 function addBook(event){
     const title = ui.title.value
     const author = ui.author.value
@@ -12,10 +17,25 @@ function addBook(event){
     const book = new Book(title,author,isbn)
     ui.addBook(book)
     ls.addBook(book)
-    console.log(book)
 
     ui.title.value = ''
     ui.author.value = ''
     ui.isbn.value = ''
     event.preventDefault()
+}
+function getBooks(){
+    // get data from LS
+    const books = ls.getData('books')
+    // for each book in books
+    books.forEach(function (booksFromLS){
+        ui.addBook(booksFromLS)
+    })
+}
+function delBook(event){
+    if (event.target.textContent === 'X') {
+        const book = ui.getBook(event.target)
+        if (ui.delBook(event.target)=== true){
+            ls.delBook(book)
+        }
+    }
 }
